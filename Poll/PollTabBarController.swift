996b41a56218ae776a4b545485d3cd40a10522ed
Jason Modisett <jason@Jasons-MacBook-Pro.local>
2018-08-30 16:13:37 -0500

    Added VotingViewController.swift and created the Voting UI in Voting.storyboard

---
+++ Poll/PollTabBarController.swift
@@ -0,0 +1,17 @@
+//
+//  PollTabBarController.swift
+//  Poll
+//
+//  Created by Jason Modisett on 8/30/18.
+//  Copyright © 2018 Jason Modisett. All rights reserved.
+//
+
+import UIKit
+
+class PollTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
---
+++ Poll/VotingViewController.swift
@@ -0,0 +1,37 @@
+//
+//  VotingViewController.swift
+//  Poll
+//
+//  Created by Jason Modisett on 8/30/18.
+//  Copyright © 2018 Jason Modisett. All rights reserved.
+//
+
+import UIKit
+
+class VotingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func submitVote(_ sender: Any) {
+        
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var responseTextField: UITextField!
+    
+
+}
