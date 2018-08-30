150ed8ba32199a6c14b5fb9841e77b51949904e1
Daniela Parra <dyparra47@gmail.com>
2018-08-30 16:34:06 -0400

    Set up the model, model controllers and some storyboards.

---
+++ Poll/VotingViewController.swift
@@ -0,0 +1,27 @@
+//
+//  VotingViewController.swift
+//  Poll
+//
+//  Created by Daniela Parra on 8/30/18.
+//  Copyright © 2018 Daniela Parra. All rights reserved.
+//
+
+import UIKit
+
+class VotingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func submitResponse(_ sender: Any) {
+    }
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var responseTextField: UITextField!
+    
+
+}
