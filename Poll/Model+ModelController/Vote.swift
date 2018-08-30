d64fdd4f24a8b413f2546291ec56d7f9695ffc5a
Jason Modisett <jason@Jasons-MacBook-Pro.local>
2018-08-30 15:57:16 -0500

    Added vote model and vote controller class

---
+++ Poll/Model+ModelController/Vote.swift
@@ -0,0 +1,14 @@
+//
+//  Poll.swift
+//  Poll
+//
+//  Created by Jason Modisett on 8/30/18.
+//  Copyright © 2018 Jason Modisett. All rights reserved.
+//
+
+import Foundation
+
+struct Vote {
+    let name: String
+    let response: String
+}
