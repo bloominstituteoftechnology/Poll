0bc59c39abf3ec2cfea7533361c5d61d3f24ea16
Ilqar Ilyasov <imilqar@gmail.com>
2018-08-30 16:49:47 -0500

    Created Combined.storyboard and CombinedViewController.swift files. Added 2 container view and 2 storyboard referance. Gave identifier to each embed segue and tied to related storyboards. Added missing constraints.

---
+++ Poll/Poll/Controllers/CombinedViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CombinedViewController.swift
+//  Poll
+//
+//  Created by Ilgar Ilyasov on 8/30/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class CombinedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
