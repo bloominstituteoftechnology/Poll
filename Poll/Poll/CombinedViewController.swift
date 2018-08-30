71f32835219e45c6b40488437c94e61700cb3a78
Farhan Farooqui <farhan@farooqui.net>
2018-08-30 17:56:31 -0400

    storyboards ready

---
+++ Poll/Poll/CombinedViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CombinedViewController.swift
+//  Poll
+//
+//  Created by Farhan on 8/30/18.
+//  Copyright © 2018 Farhan. All rights reserved.
+//
+
+import UIKit
+
+class CombinedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Poll/Poll/Vote.swift
@@ -0,0 +1,14 @@
+//
+//  Vote.swift
+//  Poll
+//
+//  Created by Farhan on 8/30/18.
+//  Copyright © 2018 Farhan. All rights reserved.
+//
+
+import Foundation
+
+struct Vote: Equatable {
+    let name: String
+    let response: String
+}
