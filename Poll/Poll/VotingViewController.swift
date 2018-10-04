fde6c12c4417a22ef09466171389971c61b89064
OmegaWulf <nikcxz@live.com>
2018-10-04 16:45:33 -0400

    set up storyboards

---
+++ Poll/Poll/VotingViewController.swift
@@ -0,0 +1,41 @@
+//
+//  VotingViewController.swift
+//  Poll
+//
+//  Created by Nikita Thomas on 10/4/18.
+//  Copyright © 2018 Nikita Thomas. All rights reserved.
+//
+
+import UIKit
+
+class VotingViewController: UIViewController {
+    
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var responseField: UITextField!
+    
+    @IBAction func submitButton(_ sender: Any) {
+        
+    }
+    
+    
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
