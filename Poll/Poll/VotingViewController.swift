658c501410fed7fb98cfd0a2d94040c7ea14b460
Paul Yi <yipaulc@gmail.com>
2018-10-04 14:18:35 -0700

    Create Voting.storyboard

---
+++ Poll/Poll/VotingViewController.swift
@@ -0,0 +1,40 @@
+//
+//  VotingViewController.swift
+//  Poll
+//
+//  Created by Paul Yi on 10/4/18.
+//  Copyright © 2018 Paul Yi. All rights reserved.
+//
+
+import UIKit
+
+class VotingViewController: UIViewController {
+
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var responseTextField: UITextField!
+    
+    
+    @IBOutlet weak var submitButtonTapped: UIButton!
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
