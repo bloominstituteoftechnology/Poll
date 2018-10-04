f38200c2d7b172cb063a433523cc9c8b59edf2f8
jWarrenDev <Jerrick.Warren@gmail.com>
2018-10-04 16:22:39 -0700

    created Model Object

---
+++ Polling App/Polling App/Vote.swift
@@ -0,0 +1,20 @@
+//
+//  Vote.swift
+//  Polling App
+//
+//  Created by Welinkton on 10/4/18.
+//  Copyright © 2018 Jerrick Warren. All rights reserved.
+//
+
+import Foundation
+
+class Vote {
+    var name: String
+    var response: String
+    
+    init(name:String, response:String) {
+        self.name = name
+        self.response = response
+    }
+}
+
