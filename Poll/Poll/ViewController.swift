ea43e207bcf6aa688837a9109ad0419b0e5d84e9
Vijay Das <vijaytdas@gmail.com>
2018-10-04 18:21:00 -0500

    Created Poll project

---
+++ Poll/Poll/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Poll
+//
+//  Created by Vijay Das on 10/4/18.
+//  Copyright © 2018 Vijay Das. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
