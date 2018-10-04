0e482145388459da3132d890d5bb864d63b01a1c
jWarrenDev <Jerrick.Warren@gmail.com>
2018-10-04 15:55:46 -0700

    initial Commit

---
+++ Polling App/Polling App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Polling App
+//
+//  Created by Welinkton on 10/4/18.
+//  Copyright © 2018 Jerrick Warren. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
