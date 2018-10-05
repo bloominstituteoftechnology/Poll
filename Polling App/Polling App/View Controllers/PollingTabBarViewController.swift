2d5ee9f57361f38842215379230466ed32806ac9
jWarrenDev <Jerrick.Warren@gmail.com>
2018-10-04 21:58:00 -0700

    created protocol

---
+++ Polling App/Polling App/View Controllers/PollingTabBarViewController.swift
@@ -0,0 +1,15 @@
+//
+//  PollingTabBarViewController.swift
+//  Polling App
+//
+//  Created by Welinkton on 10/4/18.
+//  Copyright © 2018 Jerrick Warren. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PollingTabBarViewController: UITabBarController {
+    
+    
+}
