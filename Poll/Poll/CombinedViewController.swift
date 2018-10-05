fdfd4f98f5a1f07e47fadf5ab86bfc8ba7e7e93b
Moses Robinson <mosesjrobinson@icloud.com>
2018-10-04 20:25:55 -0700

    more iterations....

--- Poll/Poll/CombinedViewController.swift
+++ Poll/Poll/CombinedViewController.swift
@@ -3,5 +3,6 @@ import UIKit
 class CombinedViewController: UIViewController {
 
 
+    
     }
 
---
+++ Poll/Poll/VoteControllerProtocol.swift
@@ -0,0 +1,5 @@
+import UIKit
+
+protocol VoteControllerProtocol {
+    var voteController: VoteController? { get set }
+}
