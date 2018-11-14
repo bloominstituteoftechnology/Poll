591334733f50f0a8e73aa05f03fbbae69548023b
Lotanna <sugabelly@gmail.com>
2018-11-13 21:16:47 -0500

    redo

---
+++ Poll/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Poll
+//
+//  Created by Lotanna Igwe-Odunze on 11/13/18.
+//  Copyright © 2018 Lotanna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
