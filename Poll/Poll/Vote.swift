f2bce60e3f50a89f68fb6a3f38fc59712b056a16
jayfenam <jonathanferrer95@gmail.com>
2019-04-22 21:02:25 -0400

    Add files via upload

---
+++ Poll/Poll/Vote.swift
@@ -0,0 +1,20 @@
+//
+//  Vote.swift
+//  Poll
+//
+//  Created by Jonathan Ferrer on 4/22/19.
+//  Copyright © 2019 Jonathan Ferrer. All rights reserved.
+//
+
+import Foundation
+
+class Vote{
+    let name: String
+    let response: String
+    
+    init(name: String, response: String) {
+        self.name = name
+        self.response = response
+    }
+    
+}
